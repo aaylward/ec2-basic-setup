fd2fb9a4a5403cc14ba996f2342a539876ded0e7
Andy Aylward <aaylward@gmail.com>
2014-11-30 18:46:01 -0500

    cite sources :kimono:

--- java.sh
+++ java.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+# cribbed from http://tecadmin.net/install-java-8-on-centos-rhel-and-fedora/
+
 cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz"
 
